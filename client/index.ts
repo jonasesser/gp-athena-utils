3cbbdb958dd98d86a47e95bc47251e9e142669b9
jonasesser <jonas.esser@gmail.com>
2023-04-13 23:17:32 +0200

    #1 fix references

--- client/index.ts
+++ client/index.ts
@@ -1,10 +1,5 @@
 import * as alt from 'alt-client';
-import { InventoryUtil } from './src/inventoryUtil';
-import './src/gpUtilsClient';
 import { getClosestPlayer, getClosestVehicle } from './src/utility/closest';
-
-//Not needed anymore
-// InventoryUtil.init();
 
 declare global {
     export interface ClientPluginAPI {
