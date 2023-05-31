0c61526e3c7ffc313523499d4ff46ffabf052490
j0n4s <jonas.esser@gmail.com>
2023-05-31 02:15:41 +0200

    #4 player override char data

--- server/overrides/player.ts
+++ server/overrides/player.ts
@@ -1,6 +1,7 @@
 import { Player } from 'alt-server';
 import * as Athena from '@AthenaServer/api';
 import * as charRef from '@AthenaShared/interfaces/character';
+import { deepCloneObject } from '@AthenaShared/utility/deepCopy';
 
 /**
  * @deprecated replace data
@@ -8,9 +9,19 @@ import * as charRef from '@AthenaShared/interfaces/character';
 declare module 'alt-server' {
     export interface Player {
         /**
-         * @deprecated: Use Athena.document.character.get instead!
+         * @deprecated:
+         * Use Athena.document.character.get instead!
+         * This is a temporary fix. Each call will execute Athena.document.character.get in background.
+         *
+         * Alternatively you can use Player.character to get a NOT up to date copy of the character. (Maybe better in performace)
          */
         data: charRef.Character;
+
+        /**
+         * This is a temporary fix and data contains a NOT up to date copy of the character after character selection
+         * Instead you can use Athena.document.character.get for a up to date character.
+         */
+        character: charRef.Character;
     }
 }
 
@@ -20,3 +31,14 @@ Object.defineProperty(Player.prototype, 'data', {
         return data;
     },
 });
+
+export class PlayerExtension {
+    static init() {
+        Athena.player.events.on('selected-character', PlayerExtension.initPlayer);
+    }
+
+    static initPlayer(player: Player) {
+        const character = Athena.document.character.get(player);
+        player.character = deepCloneObject(character);
+    }
+}
